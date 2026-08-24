f4abfaf659f5ee0232c71669b1027cdf42e28f89
Amine Dkhili <dkhili.amine@gmail.com>
2026-08-24 15:47:53 +0200

    feat: add channel-listing and marketplace-sync-event content types

---
+++ backend/src/api/marketplace-sync-event/routes/marketplace-sync-event.ts
@@ -0,0 +1,3 @@
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::marketplace-sync-event.marketplace-sync-event');
---
+++ backend/src/api/marketplace-sync-event/services/marketplace-sync-event.ts
@@ -0,0 +1,3 @@
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::marketplace-sync-event.marketplace-sync-event');
